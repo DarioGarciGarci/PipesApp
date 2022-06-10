38dd3416b8d48cfd0ed844bf8949f4389b719a37
Darío <dariogarcigarci@gmail.com>
2022-06-10 12:42:47 +0200

    PrimeFlex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
 
 //Personalizados
+import { AppRouterModule } from './app-router.module';
 import { SharedModule } from './shared/shared.module';
-
+import { VentasModule } from './ventas/ventas.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +15,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRouterModule,
-    SharedModule
+    SharedModule,
+    VentasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
